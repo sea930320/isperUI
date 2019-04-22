2741a13959b118584d498efff0b7aa6e0e7480d6
SKYDEV <sea930320@hotmail.com>
2019-04-22 20:12:56 +0800

    top-header style

--- src/views/layout/Manager.vue
+++ src/views/layout/Manager.vue
@@ -2,9 +2,9 @@
   <div class="manager-container main-bg">
     <TopHeader>
       <template v-slot:pageMenu>
-        <b-nav-item to="/manager/workflow">流程管理</b-nav-item>
-        <b-nav-item to="/manager/project">项目管理</b-nav-item>
-        <b-nav-item to="/manager/group">集群管理</b-nav-item>
+        <b-nav-item to="/manager/workflow" :active="activatedRoute=='workflow'">流程管理</b-nav-item>
+        <b-nav-item to="/manager/project" :active="activatedRoute=='project'">项目管理</b-nav-item>
+        <b-nav-item to="/manager/group" :active="activatedRoute=='group'">集群管理</b-nav-item>
         <b-nav-item to="#">用户管理</b-nav-item>
         <b-nav-item to="#">课程管理</b-nav-item>
       </template>
@@ -20,6 +20,25 @@ export default {
   components: {
     TopHeader
   },
-  created() {}
+  data() {
+    return {
+      activatedRoute: "workflow"
+    };
+  },
+  created() {},
+  watch: {
+    $route: "checkActiveRoute"
+  },
+  methods: {
+    checkActiveRoute(to) {
+      if (to.path.indexOf("workflow") !== -1) {
+        this.activatedRoute = "workflow";
+      } else if (to.path.indexOf("project") !== -1) {
+        this.activatedRoute = "project";
+      } else if (to.path.indexOf("group") !== -1) {
+        this.activatedRoute = "group";
+      }
+    }
+  }
 };
 </script>
