4eaf107d2af32b64ea5efab58e4682e8b2d04146
BlueClouDragon <gbluecloudragon@gmail.com>
2019-06-21 11:37:19 +0800

    animation node end feature

--- src/components/business/modal/endNodeHandle.vue
+++ src/components/business/modal/endNodeHandle.vue
@@ -40,10 +40,9 @@
                                     <colgroup>
                                         <col width="10%">
                                         <col width="20%">
-                                        <col width="20%">
+                                        <col width="30%">
+                                        <col width="30%">
                                         <col width="10%">
-                                        <col width="20%">
-                                        <col width="20%">
                                     </colgroup>
                                     <thead>
                                     <tr>
