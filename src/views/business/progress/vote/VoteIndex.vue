dd2acd576469ca3a22dfdb2dcae26893d4496f96
BlueClouDragon <gbluecloudragon@gmail.com>
2019-06-26 18:07:57 +0800

    bug fix and vote module

--- src/views/business/progress/vote/VoteIndex.vue
+++ src/views/business/progress/vote/VoteIndex.vue
@@ -2,7 +2,7 @@
     <div class="vote-index container mt-5">
         <loading v-if="isRunning"></loading>
         <!-- Tabs with card integration -->
-        <b-card no-body>
+        <b-card no-body v-if="status === 1">
             <b-tabs v-model="tabIndex" class="nav-fill" card>
                 <b-tab title="General">
                     <b-button class="modeBtn" :variant="(voteMode === 1) ? 'primary' : 'outline-primary'" @click="modeSelected(1)">择一表决</b-button>
@@ -11,14 +11,50 @@
                     <b-button class="modeBtn" :variant="(voteMode === 4) ? 'primary' : 'outline-primary'" @click="modeSelected(4)">自由表决</b-button>
                 </b-tab>
                 <b-tab title="Edit profile" :disabled="voteMode === null">
-                    I'm the second tab
-                    <b-card>I'm the card in tab</b-card>
+                    <b-card>
+                        <b-form class="text-left">
+                            <b-row>
+                                <b-col cols="4">
+                                    <b-form-group
+                                            id="input-group-0"
+                                            label="输入表决支 :"
+                                            label-for="input-0"
+                                            description="候选项（可以增加）"
+                                    >
+                                        <vue-tags-input
+                                                v-model="tag"
+                                                :tags="voteData.voteItems"
+                                                @tags-changed="newTags => voteData.voteItems = newTags"
+                                                placeholder="+"
+                                        />
+                                    </b-form-group>
+                                </b-col>
+                                <b-col cols="8">
+                                    <b-form-group id="input-group-1" label="表决主题 :" label-for="input-1">
+                                        <b-form-input
+                                                id="input-1"
+                                                v-model="voteData.voteTitle"
+                                                required
+                                                placeholder="请入主题"
+                                        ></b-form-input>
+                                    </b-form-group>
+
+                                    <b-form-group id="input-group-2" label="表决说明 :" label-for="input-2">
+                                        <b-form-input
+                                                id="input-2"
+                                                v-model="voteData.voteDescription"
+                                                required
+                                                placeholder="请入说明"
+                                        ></b-form-input>
+                                    </b-form-group>
+                                </b-col>
+                            </b-row>
+                        </b-form>
+                    </b-card>
                     <!-- Control buttons-->
-                    <div class="text-center">
-                        <b-button-group class="mt-2">
-                            <b-button @click="tabIndex--">Previous</b-button>
-                            <b-button @click="tabIndex++">Next</b-button>
-                        </b-button-group>
+                    <div class="text-center mt-4">
+                        <b-button variant="primary" @click="tabIndex--" class="mr-5">下一步</b-button>
+                        <b-button variant="primary" @click="tabIndex++">上一步</b-button>
                     </div>
                 </b-tab>
                 <b-tab title="Info" :disabled="check_voteData">I'm the last tab</b-tab>
@@ -31,13 +67,16 @@
     import {mapState} from "vuex";
     import Loading from "@/components/loading/Loading";
     import VoteService from "@/services/voteService";
+    import VueTagsInput from '@johmun/vue-tags-input';
     export default {
         name: "vote-index",
         components: {
             Loading,
+            VueTagsInput,
         },
         data() {
             return {
+                tag: '',
                 status: null,
                 tabIndex: 1,
                 voteMode: null,
@@ -64,7 +103,7 @@
                 return this.meta.currentRoleAllocation;
             },
             check_voteData() {
-                return true;
+                return !!!(this.voteData.voteItems.length > 1 && this.voteData.voteTitle && this.voteData.voteDescription);
             }
         },
         created() {
