b6d5bf70b89c098dc867c451d02898927fcec374
BlueClouDragon <gbluecloudragon@gmail.com>
2019-07-19 01:34:21 +0800

    course version 2

--- src/services/courseService.js
+++ src/services/courseService.js
@@ -39,6 +39,66 @@ class CourseService {
             params: data
         })
     }
+    /**
+     * 获取流程列表
+     * @param data
+     * @returns {{pre, visitor}|*}
+     */
+    saveEditCourse(data) {
+        return xhr({
+            method: 'post',
+            url: '/course/save_edit',
+            params: data
+        })
+    }
+    /**
+     * 获取流程列表
+     * @param data
+     * @returns {{pre, visitor}|*}
+     */
+    getTeacherList(data) {
+        return xhr({
+            method: 'get',
+            url: '/course/get_teacher_list',
+            params: data
+        })
+    }
+    /**
+     * 获取流程列表
+     * @param data
+     * @returns {{pre, visitor}|*}
+     */
+    teacherChangeSave(data) {
+        return xhr({
+            method: 'post',
+            url: '/course/save_teacher_change',
+            params: data
+        })
+    }
+    /**
+     * 获取流程列表
+     * @param data
+     * @returns {{pre, visitor}|*}
+     */
+    deleteCourse(data) {
+        return xhr({
+            method: 'post',
+            url: '/course/delete_course',
+            params: data
+        })
+    }
+    /**
+     * 获取流程列表
+     * @param data
+     * @returns {{pre, visitor}|*}
+     */
+    excelDataSave(data) {
+        return xhr({
+            method: 'post',
+            url: '/course/excel_data_save',
+            params: data
+        })
+    }
 
 }
 
